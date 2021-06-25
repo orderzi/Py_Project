7c562b78338dffafe2f0d625243e53b9a3ec3edb
orderzi <59146191+orderzi@users.noreply.github.com>
2021-06-25 12:29:54 +0300

    writing e2e test

---
+++ e2e.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.common.exceptions import *
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('headless')
+driver = webdriver.Chrome('C:\chromedriver.exe', options=chrome_options)
+# Run selenium on chrome without opening it
+
+def test_scores_service(app_url):
+    try:
+        website_score = driver.get(app_url)
+        website_score = int(driver.find_element_by_id('score').text)
+    except WebDriverException:
+        print('Site is not reachble')
+        return False
+    try:
+        assert 1 <= website_score <= 1000, 'Value must be 1-1000'
+        return True
+    except AssertionError as msg:
+        print(msg)
+        return False
+
+def main():
+    call_test = test_scores_service('http://localhost:5001/')
+    if call_test:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    final = main()
+    print(final)
