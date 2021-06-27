18800bdda9c697d45e3498fe24126ecd7c694945
orderzi <59146191+orderzi@users.noreply.github.com>
2021-06-27 18:23:42 +0300

    Changing source code to fit linux systems

--- Utils.py
+++ Utils.py
@@ -1,7 +1,7 @@
 import os
 
 cwd = os.getcwd()
-scores_file = os.path.join(cwd,'Scores\Scores.txt')
+scores_file = os.path.join(cwd,'Scores/Scores.txt')
 scores_file_name = 'Scores.txt'
 bad_return_code = -1
 
