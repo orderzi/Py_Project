bb0993cb76359494035bfeba3357fc05ce09bc3f
orderzi <59146191+orderzi@users.noreply.github.com>
2021-07-05 12:20:20 +0300

    Adding Flask_url var

--- config.py
+++ config.py
@@ -1,21 +1,18 @@
 import os
 
-# username = os.environ.get('DB_USER')
-# password = os.environ.get('DB_PASSWORD')
-# host = os.environ.get('DB_HOST')
-# database = os.environ.get('DB_DATABASE')
-
-# MY_SQL_CRED = {
-#     "username": username,
-#     "password": password,
-#     "host": host,
-#     "database": database
-# }
-
+username = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+host = os.environ.get('DB_HOST')
+database = os.environ.get('DB_DATABASE')
+flask_url = os.environ.get('FLASK_URL')
 
 MY_SQL_CRED = {
-    "username": 'root',
-    "password": 'Wdcft5432!',
-    "host": 'database-1.curueu8grydd.us-east-1.rds.amazonaws.com',
-    "database": 'flaskdb'
+    "username": username,
+    "password": password,
+    "host": host,
+    "database": database
 }
+
+
+
+
